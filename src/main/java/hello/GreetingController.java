acb900563e9a5b04c44559ddc1e4a1302a698a42
Madhubabu <msandara@salesforce.com>
2017-04-14 19:27:48 -0700

    first commit

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,16 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping(value= "/alertNotification", method = RequestMethod.POST)
+    public void alertNotification(@RequestBody String body) {
+        Gson gson  = new Gson();
+        System.out.println(body);
+    }
+}
